26a19a35b38f0f3ba9c844967d20a9e17bef4930
tsjtsj123 <1148680859@qq.com>
2022-03-22 19:21:54 +0800

    frist·

--- src/gfg01/Gjuk.java
+++
@@ -1,8 +0,0 @@
-package gfg01;
-
-public class Gjuk {
-    public static void main(String[] args) {
-        System.out.println("你好");
-        System.out.println("再见");
-    }
-}
---
+++ src/gfg01/Gjuk2323.java
@@ -0,0 +1,11 @@
+package gfg01;
+
+public class Gjuk2323 {
+    public static void main(String[] args) {
+        Gjuk2323 n = new Gjuk2323();
+        n.Gjuk1();
+    }
+   void Gjuk1(){
+        System.out.println("mm");
+    }
+}
